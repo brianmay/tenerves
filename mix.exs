b2b9f6f27789a0d399a6f7aee6ba7db4028440f2
Brian May <brian@linuxpenguins.xyz>
2019-09-13 19:24:55 +1000

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule TeNerves.MixProject do
       {:timex, "~> 3.0"},
       {:ex_tesla, "~> 0.0"},
       {:ecto_sql, "~> 3.0"},
-      {:postgrex, "~> 0.14"},
+      {:postgrex, "~> 0.15"},
       {:tortoise, "~> 0.9.2"},
       {:jason, "~> 1.1"},
       {:geocalc, "~> 0.5"}
@@ -61,7 +61,7 @@ defmodule TeNerves.MixProject do
     [
       {:nerves_runtime, "~> 0.6"},
       {:nerves_network, "~> 0.3"},
-      {:nerves_time, "~> 0.2.0"},
+      {:nerves_time, "~> 0.3"},
       {:nerves_init_gadget, "~> 0.4"},
       {:dns, "~> 2.1.2"}
     ] ++ system(target)
