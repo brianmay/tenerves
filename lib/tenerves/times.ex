7271cec6e84dba0e0690c58b4b741f0241fdf98d
Brian May <brian@linuxpenguins.xyz>
2019-10-26 18:37:11 +1100

    Reformat file

--- lib/tenerves/times.ex
+++ lib/tenerves/times.ex
@@ -7,8 +7,7 @@ defmodule TeNerves.Times do
 
   def round_time_up(date_time, interval) do
     seconds = Timex.to_unix(date_time)
-    units = seconds / interval |> Float.ceil |> trunc
+    units = (seconds / interval) |> Float.ceil() |> trunc
     Timex.from_unix(units * interval)
   end
-
 end
