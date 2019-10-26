2ada367192dddf8ac6838389eeece07817b4566d
Brian May <brian@linuxpenguins.xyz>
2019-10-26 16:30:40 +1100

    Add missing file

---
+++ lib/tenerves/times.ex
@@ -0,0 +1,14 @@
+defmodule TeNerves.Times do
+  def round_time(date_time, interval, offset) do
+    seconds = Timex.to_unix(date_time)
+    units = div(seconds, interval) + offset
+    Timex.from_unix(units * interval)
+  end
+
+  def round_time_up(date_time, interval) do
+    seconds = Timex.to_unix(date_time)
+    units = seconds / interval |> Float.ceil |> trunc
+    Timex.from_unix(units * interval)
+  end
+
+end
