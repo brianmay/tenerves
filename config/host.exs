597417fc81058f9473f68b8d46c4abad4e89dabc
Brian May <brian@linuxpenguins.xyz>
2019-11-01 11:30:52 +1100

    Add ability to configure production/test database separately

--- config/host.exs
+++ config/host.exs
@@ -2,3 +2,5 @@ use Mix.Config
 
 config :tenerves,
   ca_cert_file: Path.join(["./rootfs_overlay", System.get_env("CA_CERT_FILE")])
+
+config :tenerves, TeNerves.Repo, url: System.get_env("HOST_DATABASE_URL")
--- config/rpi3.exs
+++ config/rpi3.exs
@@ -1,3 +1,5 @@
 use Mix.Config
 
 config :tzdata, :data_dir, "/root/elixir_tzdata_data"
+
+config :tenerves, TeNerves.Repo, url: System.get_env("PROD_DATABASE_URL")
