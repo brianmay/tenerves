ed41ca884f7a19184ae7d0ca605973d180fe052c
Brian May <brian@linuxpenguins.xyz>
2020-04-10 16:13:49 +1000

    Fix tesla_token_file

--- config/rpi3.exs
+++ config/rpi3.exs
@@ -2,6 +2,8 @@ use Mix.Config
 
 config :tzdata, :data_dir, "/root/elixir_tzdata_data"
 
+config :tenerves,
+  tesla_token_file: "/root/token.json"
+
 config :tenerves, TeNerves.Repo,
-  url: System.get_env("PROD_DATABASE_URL"),
-  tesla_token_file: "./token.json"
+  url: System.get_env("PROD_DATABASE_URL")
